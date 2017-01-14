906423857a08dd85a6a31562b470be5e326523ae
Brendan McKenzie <hello@brendanmckenzie.com>
2017-01-14 16:39:43 +1100

    Debugging deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 for f in artifacts/*.nupkg
 do
     echo "Deploying $f"
-    curl -sS -X POST https://www.myget.org/F/kasbah/api/v2/package -H "X-NuGet-ApiKey: $MYGET_KEY" -F "data=@$f"
+    curl -X POST https://www.myget.org/F/kasbah/api/v2/package -H "X-NuGet-ApiKey: $MYGET_KEY" -F "data=@$f"
 done
