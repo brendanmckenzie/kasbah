1545af3bc1dd50c4390261116b2cb6bfb321f358
Brendan McKenzie <hello@brendanmckenzie.com>
2017-01-14 16:11:10 +1100

    Securing deploy CI

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 for f in artifacts/*.nupkg
 do
-    curl -X POST https://www.myget.org/F/kasbah/api/v2/package -H "X-NuGet-ApiKey: 93888c8f-2ab2-4ee6-b9fa-4c99f47f0bb9" -F "data=@artifacts/$f"
+    curl -X POST https://www.myget.org/F/kasbah/api/v2/package -H "X-NuGet-ApiKey: $MYGET_KEY" -F "data=@artifacts/$f"
 done
