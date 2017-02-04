df037ff11831601f98cf9d8ce2e0f82bc6654279
Brendan McKenzie <hello@brendanmckenzie.com>
2017-02-04 18:28:34 +1100

    Updated content tree functionality to expand to selected node on refresh

--- src/Kasbah.Web.ContentManagement.UI/src/routes/Content/components/ContentTree/index.js
+++ src/Kasbah.Web.ContentManagement.UI/src/routes/Content/components/ContentTree/index.js
@@ -47,6 +47,18 @@ class ContentTree extends React.Component {
     if (nextProps.createNode.success && nextProps.createNode.success !== this.props.createNode.success) {
       this.handleRefresh()
     }
+    if (nextProps.describeTree.success && nextProps.describeTree.success !== this.props.describeTree.success) {
+      const getNode = id => nextProps.describeTree.payload.filter(ent => ent.id === id)[0]
+      let node = getNode(this.context.router.params.id)
+      let expandedNodes = {}
+      while (node.parent !== null) {
+        expandedNodes[node.parent] = true
+        node = getNode(node.parent)
+      }
+      this.setState({
+        expandedNodes
+      })
+    }
   }
 
   getChildContext() {
