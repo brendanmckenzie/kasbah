1fa30474cc7542caedb6acb19ee6589f6bd6f2e5
Brendan McKenzie <brendan.mckenzie@deepend.com.au>
2017-02-08 15:35:22 +1100

    Migrated to MSBuild... reluctantly.

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -7,7 +7,7 @@ COMMIT_COUNT=$(git rev-list --all --count)
 COMMIT_COUNT_PADDED=$(printf "%04d" $COMMIT_COUNT)
 DOTNET_PACK_OPTS="-c Release -o ./artifacts --version-suffix=build$COMMIT_COUNT_PADDED"
 
-dotnet restore src/**/project.json
+dotnet restore
 
 dotnet pack src/Kasbah $DOTNET_PACK_OPTS
 dotnet pack src/Kasbah.DataAccess.ElasticSearch $DOTNET_PACK_OPTS
