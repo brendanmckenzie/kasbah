5de1bccc0e0b67cafb548d2034adb73bf47843eb
Brendan McKenzie <hello@brendanmckenzie.com>
2017-06-25 13:22:52 +1000

    Added option to specify build suffix

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -3,9 +3,13 @@ set -e
 rm -rf src/Kasbah.Web.ContentManagement/wwwroot
 cp -r src/Kasbah.Web.ContentManagement.UI/dist src/Kasbah.Web.ContentManagement/wwwroot
 
-COMMIT_COUNT=$(git rev-list --all --count)
-COMMIT_COUNT_PADDED=$(printf "%04d" $COMMIT_COUNT)
-DOTNET_PACK_OPTS="-c Release -o $(pwd)/artifacts --version-suffix=build$COMMIT_COUNT_PADDED"
+if [ -z "$VERSION_SUFFIX" ]; then
+    COMMIT_COUNT=$(git rev-list --all --count)
+    COMMIT_COUNT_PADDED=$(printf "%04d" $COMMIT_COUNT)
+    VERSION_SUFFIX="build$COMMIT_COUNT_PADDED"
+fi
+
+DOTNET_PACK_OPTS="-c Release -o $(pwd)/artifacts --version-suffix=$VERSION_SUFFIX"
 
 dotnet restore
 
