79a68b38ac45037466afa86dc7cd107d6862a5bf
Brendan McKenzie <hello@brendanmckenzie.com>
2017-06-26 18:04:20 +1000

    Removing very old reference to material ui

--- src/Kasbah.Web.ContentManagement.UI/src/containers/AppContainer.js
+++ src/Kasbah.Web.ContentManagement.UI/src/containers/AppContainer.js
@@ -1,34 +1,23 @@
 import React, { Component, PropTypes } from 'react'
 import { browserHistory, Router } from 'react-router'
 import { Provider } from 'react-redux'
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
-import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import injectTapEventPlugin from 'react-tap-event-plugin'
-
-injectTapEventPlugin()
-
-const muiTheme = getMuiTheme({
-  palette: { }
-})
 
 class AppContainer extends Component {
   static propTypes = {
-    routes : PropTypes.object.isRequired,
-    store  : PropTypes.object.isRequired
+    routes: PropTypes.object.isRequired,
+    store: PropTypes.object.isRequired
   }
 
-  shouldComponentUpdate () {
+  shouldComponentUpdate() {
     return false
   }
 
-  render () {
+  render() {
     const { routes, store } = this.props
 
     return (
       <Provider store={store}>
-        <MuiThemeProvider muiTheme={muiTheme}>
-          <Router history={browserHistory} children={routes} />
-        </MuiThemeProvider>
+        <Router history={browserHistory} children={routes} />
       </Provider>
     )
   }
