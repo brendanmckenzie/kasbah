f5301d2e6b3aa3070db16f3ba2044bb78257ad4a
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-06-28 13:49:17 +1000

    Added _graceful_ handling of cache errors

--- src/Kasbah/Extensions/CacheExtensions.cs
+++ src/Kasbah/Extensions/CacheExtensions.cs
@@ -9,24 +9,32 @@ namespace Kasbah.Extensions
     {
         public static async Task<T> GetOrSetAsync<T>(this IDistributedCache cache, string key, Func<Task<T>> generator)
         {
-            if (cache == null)
+            try
             {
+                if (cache == null)
+                {
+                    return await generator();
+                }
+
+                // TODO: find out why GetStringAsync/SetStringAsync don't work
+                var cacheValue = cache.GetString(key);
+                if (cacheValue == null)
+                {
+                    var generatedValue = await generator();
+
+                    cache.SetString(key, JsonConvert.SerializeObject(generatedValue));
+
+                    return generatedValue;
+                }
+                else
+                {
+                    return JsonConvert.DeserializeObject<T>(cacheValue);
+                }
+            }
+            catch
+            {
+                // TODO: log this error
                 return await generator();
-            }
-
-            // TODO: find out why GetStringAsync/SetStringAsync don't work
-            var cacheValue = cache.GetString(key);
-            if (cacheValue == null)
-            {
-                var generatedValue = await generator();
-
-                cache.SetString(key, JsonConvert.SerializeObject(generatedValue));
-
-                return generatedValue;
-            }
-            else
-            {
-                return JsonConvert.DeserializeObject<T>(cacheValue);
             }
         }
     }
