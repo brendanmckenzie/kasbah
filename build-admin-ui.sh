157e42de99246726eecfb02075a46bba943387c9
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-11-25 10:05:32 +1100

    Moving built UI files to correct location

--- build-admin-ui.sh
+++ build-admin-ui.sh
@@ -3,3 +3,9 @@ set -e
 cd src/Kasbah.Web.Management.UI
 yarn
 yarn run build
+
+cd ../../
+
+rm -rf src/Kasbah.Web/Management/Ui
+mkdir -p src/Kasbah.Web/Management/Ui
+cp -r src/Kasbah.Web.Management.UI/dist/* src/Kasbah.Web/Management/Ui
