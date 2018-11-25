6e32dfd547d3d99ffab182a8173d6903f9153432
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2018-11-25 14:32:41 +1100

    Fix duplicated build code

--- build-dotnet.sh
+++ build-dotnet.sh
@@ -1,9 +1,5 @@
 set -e
 
-rm -rf src/Kasbah.Web/Management/Ui
-mkdir -p src/Kasbah.Web/Management/Ui
-cp -r src/Kasbah.Web.Management.UI/dist/* src/Kasbah.Web/Management/Ui
-
 if [ -z "$VERSION_SUFFIX" ]; then
     COMMIT_COUNT=$(git rev-list --all --count)
     COMMIT_COUNT_PADDED=$(printf "%04d" $COMMIT_COUNT)
