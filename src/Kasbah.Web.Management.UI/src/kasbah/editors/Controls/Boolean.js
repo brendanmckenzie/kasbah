88a80a7fce8a7a10a91a3f04686f57d54b6d5770
Brendan McKenzie <brendan.mckenzie@deepend.melbourne>
2019-02-11 10:36:13 +1100

    Removed incorrect class on checkbox

--- src/Kasbah.Web.Management.UI/src/kasbah/editors/Controls/Boolean.js
+++ src/Kasbah.Web.Management.UI/src/kasbah/editors/Controls/Boolean.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-const Boolean = ({ input: { value, onChange }, className }) => (
-  <input type='checkbox' className={className} onChange={onChange} value={value} />
-)
+const Boolean = ({ input: { value, onChange } }) => <input type="checkbox" onChange={onChange} value={value} />
 
 Boolean.propTypes = {
   input: PropTypes.object.isRequired,
-  className: PropTypes.string
 }
 
 Boolean.alias = 'boolean'
